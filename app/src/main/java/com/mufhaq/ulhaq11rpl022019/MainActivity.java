25d6252a670a0511df2e0a0535092fb81fad6b04
mufHaQ <masbrowmess33@gmail.com>
2020-08-16 17:37:02 +0700

    add Control class, MainMenu class, Shared preferences

--- app/src/main/java/com/mufhaq/ulhaq11rpl022019/MainActivity.java
+++ app/src/main/java/com/mufhaq/ulhaq11rpl022019/MainActivity.java
@@ -2,13 +2,45 @@ package com.mufhaq.ulhaq11rpl022019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+
+    EditText txtusername;
+    EditText txtpassword;
+    Button btnlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pref = getSharedPreferences("login", MODE_PRIVATE);
+        txtusername = (EditText) findViewById(R.id.txtUsername);
+        txtpassword = (EditText) findViewById(R.id.txtPassword);
+        btnlogin = (Button) findViewById(R.id.btnLogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (txtusername.getText().toString().equalsIgnoreCase("admin") && txtpassword.getText().toString().equalsIgnoreCase("admin")){
+//                    save ke SP
+                    editor = pref.edit();
+                    editor.putString("username", txtusername.getText().toString());
+                    editor.putString("status", "login");
+                    editor.apply();
+//                    menuju MainMenu
+                    startActivity(new Intent(getApplicationContext(), MainMenu.class));
+                    finish();
+                }
+            }
+        });
     }
 }
