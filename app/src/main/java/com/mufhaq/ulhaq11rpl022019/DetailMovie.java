112b1e6242151b6108558905e5d2a1b46eec8d31
Ulhaq <masbrowmess33@gmail.com>
2020-09-24 19:21:22 +0700

    Move apply & Save to realm full

--- app/src/main/java/com/mufhaq/ulhaq11rpl022019/DetailMovie.java
+++ app/src/main/java/com/mufhaq/ulhaq11rpl022019/DetailMovie.java
@@ -3,6 +3,7 @@ package com.mufhaq.ulhaq11rpl022019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -10,7 +11,14 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.Target;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class DetailMovie extends AppCompatActivity {
+
+    Realm realm;
+    RealmHelper realmHelper;
+    ModelMovieRealm movieModel;
 
     Bundle extras;
     String title;
@@ -51,6 +59,27 @@ public class DetailMovie extends AppCompatActivity {
                     .into(ivposter);
             // and get whatever type user account id is
         }
+
+        //Set up Realm
+        Realm.init(DetailMovie.this);
+        RealmConfiguration configuration = new RealmConfiguration.Builder().build();
+        realm = Realm.getInstance(configuration);
+
+
+        btnbookmark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                movieModel = new ModelMovieRealm();
+                movieModel.setDesc(description);
+                movieModel.setJudul(title);
+                movieModel.setPath(path);
+                movieModel.setReleaseDate(date);
+
+                realmHelper = new RealmHelper(realm);
+                realmHelper.save(movieModel);
+
+            }
+        });
     }
 
 
